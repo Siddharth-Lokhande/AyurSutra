c515aef3ab0d4d2aed82c12042e8fc6638646291
Siddharth <sidlokhande2504@gmail.com>
2025-09-05 00:41:45 +0530

    mit

---
+++ src/main/java/com/ayursutra/panchakarma/PanchakarmaApplication.java
@@ -0,0 +1,13 @@
+package com.ayursutra.panchakarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanchakarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanchakarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayursutra/panchakarma/PanchakarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayursutra.panchakarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanchakarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
